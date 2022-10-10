e621148653bd2ae1e191cf58d71b5dc374650886
towsifzilani <towsifzilani@gmail.com>
2022-10-11 03:20:13 +0600

    dynamic views

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,14 @@ Route::get('/', function(){
 	return view('welcome');
 });
 
+Route::get('/about', function(){
+	return view('about');
+});
+
+Route::get('/pricing', function(){
+	return view('pricing');
+});
+
 Route::get('/contact', function(){
 	return view('contact');
 });
