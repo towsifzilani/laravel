cd8c81b6a2abca42b96e655b4dd9f9957be6ee58
Abdul Kader Zilani <26829777+towsifzilani@users.noreply.github.com>
2022-10-11 17:45:51 +0600

    Add files via upload

---
+++ views/layout.blade.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>eStartup Bootstrap Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Roboto:100,300,400,500,700|Philosopher:400,400i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: eStartup - v4.9.1
+  * Template URL: https://bootstrapmade.com/estartup-bootstrap-landing-page-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+<header id="header" class="header fixed-top d-flex align-items-center">
+  <div class="container d-flex align-items-center justify-content-between">
+
+    <div id="logo">
+      <h1><a href="index.html"><span>e</span>Startup</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" title="" /></a>-->
+    </div>
+
+    <nav id="navbar" class="navbar">
+      <ul>
+        <li><a class="nav-link scrollto {{ Request::is('/') ? 'active':'' }}" href="/">Home</a></li>
+        <li><a class="nav-link scrollto {{ Request::is('about') ? 'active':'' }}" href="/about">About</a></li>
+        <li><a class="nav-link scrollto" href="/#features">Features</a></li>
+        <li><a class="nav-link scrollto {{ Request::is('pricing') ? 'active':'' }}" href="/pricing">Pricing</a></li>
+        <li><a class="nav-link scrollto {{ Request::is('blog') ? 'active':'' }}" href="/blog">Blog</a></li>
+        {{--<li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+          <ul>
+            <li><a href="#">Drop Down 1</a></li>
+            <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+              <ul>
+                <li><a href="#">Deep Drop Down 1</a></li>
+                <li><a href="#">Deep Drop Down 2</a></li>
+                <li><a href="#">Deep Drop Down 3</a></li>
+                <li><a href="#">Deep Drop Down 4</a></li>
+                <li><a href="#">Deep Drop Down 5</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Drop Down 2</a></li>
+            <li><a href="#">Drop Down 3</a></li>
+            <li><a href="#">Drop Down 4</a></li>
+          </ul>
+        </li>--}}
+        <li><a class="nav-link scrollto" href="/contact">Contact</a></li>
+      </ul>
+      <i class="bi bi-list mobile-nav-toggle"></i>
+    </nav><!-- .navbar -->
+
+  </div>
+</header><!-- End Header -->
+
+  @yield('content')
+
+  <!-- ======= Newsletter Section ======= -->
+  <section id="newsletter" class="newsletter text-center">
+    <div class="overlay padd-section">
+      <div class="container" data-aos="zoom-in">
+
+        <div class="row justify-content-center">
+          <div class="col-md-9 col-lg-6">
+            <form class="d-flex" method="POST" action="#">
+              <input type="email" class="form-control " placeholder="Email Adress" name="email">
+              <button type="submit" class="btn btn-default"><i class="bi bi-location-arrow"></i>Subscribe</button>
+            </form>
+          </div>
+        </div>
+
+        <ul class="list-unstyled">
+          <li><a href="#"><i class="bi bi-facebook"></i></a></li>
+          <li><a href="#"><i class="bi bi-twitter"></i></a></li>
+          <li><a href="#"><i class="bi bi-instagram"></i></a></li>
+          <li><a href="#"><i class="bi bi-linkedin"></i></a></li>
+        </ul>
+
+      </div>
+    </div>
+  </section><!-- End Newsletter Section -->
+
+  <!-- ======= Footer ======= -->
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-12 col-lg-4">
+          <div class="footer-logo">
+
+            <a class="navbar-brand" href="#">eStartup</a>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
+
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-3 col-lg-2">
+          <div class="list-menu">
+
+            <h4>Abou Us</h4>
+
+            <ul class="list-unstyled">
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Features item</a></li>
+              <li><a href="#">Live streaming</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+            </ul>
+
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-3 col-lg-2">
+          <div class="list-menu">
+
+            <h4>Abou Us</h4>
+
+            <ul class="list-unstyled">
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Features item</a></li>
+              <li><a href="#">Live streaming</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+            </ul>
+
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-3 col-lg-2">
+          <div class="list-menu">
+
+            <h4>Support</h4>
+
+            <ul class="list-unstyled">
+              <li><a href="#">faq</a></li>
+              <li><a href="#">Editor help</a></li>
+              <li><a href="#">Contact us</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+            </ul>
+
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-3 col-lg-2">
+          <div class="list-menu">
+
+            <h4>Abou Us</h4>
+
+            <ul class="list-unstyled">
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Features item</a></li>
+              <li><a href="#">Live streaming</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+            </ul>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="copyrights">
+      <div class="container">
+        <p>&copy; Copyrights eStartup. All rights reserved.</p>
+        <div class="credits">
+          <!--
+          All the links in the footer should remain intact.
+          You can delete the links only if you purchased the pro version.
+          Licensing information: https://bootstrapmade.com/license/
+          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=eStartup
+        -->
+          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div>
+      </div>
+    </div>
+
+  </footer><!-- End  Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ views/pricing.blade.php
@@ -0,0 +1,94 @@
+@extends('layout')
+
+@section('content')
+  <!-- ======= Pricing Section ======= -->
+  <section id="pricing" class="padd-section text-cente">
+
+    <div class="container" data-aos="fade-up">
+      <div class="section-title text-center">
+
+        <h2>Meet With Price</h2>
+        <p class="separator">Integer cursus bibendum augue ac cursus .</p>
+      </div>
+
+      <div class="row" data-aos="fade-up" data-aos-delay="100">
+
+        <div class="col-md-6 col-lg-3">
+          <div class="block-pricing">
+            <div class="pricing-table">
+              <h4>basic</h4>
+              <h2>$29</h2>
+              <ul class="list-unstyled">
+                <li><b>4 GB</b> Ram</li>
+                <li><b>7/24</b> Tech Support</li>
+                <li><b>40 GB</b> SSD Cloud Storage</li>
+                <li>Monthly Backups</li>
+                <li>Palo Protection</li>
+              </ul>
+              <div class="table_btn">
+                <a href="#" class="btn"><i class="bi bi-cart"></i> Buy Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3">
+          <div class="block-pricing">
+            <div class="pricing-table">
+              <h4>PERSONAL</h4>
+              <h2>$29</h2>
+              <ul class="list-unstyled">
+                <li><b>4 GB</b> Ram</li>
+                <li><b>7/24</b> Tech Support</li>
+                <li><b>40 GB</b> SSD Cloud Storage</li>
+                <li>Monthly Backups</li>
+                <li>Palo Protection</li>
+              </ul>
+              <div class="table_btn">
+                <a href="#" class="btn"><i class="bi bi-cart"></i> Buy Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3">
+          <div class="block-pricing">
+            <div class="pricing-table">
+              <h4>BUSINESS</h4>
+              <h2>$29</h2>
+              <ul class="list-unstyled">
+                <li><b>4 GB</b> Ram</li>
+                <li><b>7/24</b> Tech Support</li>
+                <li><b>40 GB</b> SSD Cloud Storage</li>
+                <li>Monthly Backups</li>
+                <li>Palo Protection</li>
+              </ul>
+              <div class="table_btn">
+                <a href="#" class="btn"><i class="bi bi-cart"></i> Buy Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3">
+          <div class="block-pricing">
+            <div class="pricing-table">
+              <h4>profeesional</h4>
+              <h2>$29</h2>
+              <ul class="list-unstyled">
+                <li><b>4 GB</b> Ram</li>
+                <li><b>7/24</b> Tech Support</li>
+                <li><b>40 GB</b> SSD Cloud Storage</li>
+                <li>Monthly Backups</li>
+                <li>Palo Protection</li>
+              </ul>
+              <div class="table_btn">
+                <a href="#" class="btn"><i class="bi bi-cart"></i> Buy Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section><!-- End Pricing Section -->
+@endsection
